576ea316493c06a62c22bc9bc4997ea4297eb174
Koki Akamatsu <list6311@gmail.com>
2020-09-17 14:15:39 +0900

    study

--- day1.py
+++ day1.py
@@ -18,9 +18,29 @@ for i in range(2,1001):
         print(i)
 print('------------------------------------------------------------')
 # problem 3. Display the 10th Fibonacci sequence 
-
+a = 0
+b = 1
+for i in range(10):
+    print(b)
+    a, b = b, a+b
 print('------------------------------------------------------------')
 # problem 4. Display the least common multiple/greatest common divisor of two natural numbers
+def gcd(a,b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b,a%b) 
+
+a = 16
+b = 6        
+xab = gcd(a,b)
+print(xab)
+
+zab = a*b/xab
+print(zab)
 print('------------------------------------------------------------')
 # problem 5. Multiples of 3 less than 100
+for i in range(0,101):
+    if "3" in str(i) or 0 == i % 3:
+        print(i)
 print('------------------------------------------------------------')
